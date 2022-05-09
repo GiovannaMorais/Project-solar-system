d8e4d0e82a6afb3586dbb0b5db9fe6625cc0266b
GiovannaMorais <gigimorais9@hotmail.com>
2022-05-09 16:55:22 -0300

    Requisitos 2,3,4,5,6

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import Header from './components/Header';
+import SolarSystem from './components/SolarSystem';
+import planets from './data/planets';
 
 class App extends React.Component {
   render() {
     return (
-      <Header />
+      <div>
+        <Header />
+        <SolarSystem planets={ planets } />
+      </div>
     );
   }
 }
